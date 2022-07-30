1c2f8e62bd3cf12090fae2d19d2ba46b261cd6e6
Yohanes <yohanesmulugeta21@gmail.com>
2022-07-30 17:07:01 +0300

    shift method is added to the linkedlist to allow deleting node from the start

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ class LInkedList {
     this.length = 1;
   }
 
+  // -------------------------------------------------------------------------------------------- ADDING node to the END of linked list
   push(value) {
     const newNode = new Node(value);
 
-    // -------------------------------------- checking if the head and the tail are pointing to null
+    // --------------- checking if the head and the tail are pointing to null
     if (!this.head) this.head = newNode;
     else this.tail.next = newNode;
     this.tail = newNode;
@@ -25,37 +26,62 @@ class LInkedList {
     return this; // this is the intire linkied list
   }
 
+  //   ----------------------------------------------------------------------------------------- ADDING node to the start of the linked list
   unshift(value) {
     const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
 
+    // ----------------- checking if the linked list was empity
+    if (!this.head.next) this.tail = this.head;
+
     return this; // this is the entire underlined linkedlist
   }
 
-  pop() {
-    if (!this.head) return null;
+  //   -------------------------------------------------------------------------------------------- DELETING node from the start of the linked list
+
+  shift() {
+    if (!this.head) return undefined;
+
+    this.head = this.head.next;
+
+    if (this.length === 1) {
+      this.tail = null;
+    }
 
     this.length--;
 
+    return this;
+  }
+
+  // ------------------------------------------------------------------------------------------- DELETING node to the END of the linked list
+  pop() {
+    // ----------------- for empity linked list
+    if (!this.head) return undefined;
+
+    this.length--;
+
+    // ---------------- for linked list with only one node
     if (!this.head.next) {
       this.head = null;
       this.tail = this.head;
-
-      return null;
     }
 
-    let curNode = this.head;
+    // --------------- for linked list with > 1 node
+    let pre = this.head; // previous node
+    let temp = this.head;
 
-    while (true) {
-      if (!curNode.next.next) {
-        this.tail = curNode;
+    while (temp) {
+      temp = pre.next;
+      //   ------------- if this node is the final
+      if (!temp.next) {
+        this.tail = pre;
         this.tail.next = null;
         break;
       }
 
-      curNode = curNode.next;
+      pre = pre.next;
     }
 
     return this;
@@ -66,8 +92,10 @@ let myLinkedList = new LInkedList(4);
 
 myLinkedList.push(5);
 myLinkedList.push(7);
-myLinkedList.unshift(3);
 
-myLinkedList.pop();
+// myLinkedList.pop();
+myLinkedList.unshift(3);
+myLinkedList.shift();
+// myLinkedList.pop();
 
 console.log(myLinkedList);
