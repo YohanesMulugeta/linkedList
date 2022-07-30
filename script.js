d9a3900aacc1e4cf2323db1bb78a48d1a66414e3
Yohanes <yohanesmulugeta21@gmail.com>
2022-07-30 15:33:40 +0300

    pop method is added to the linkedlist

--- script.js
+++ script.js
@@ -15,10 +15,14 @@ class LInkedList {
 
   push(value) {
     const newNode = new Node(value);
-    if (this.length === 0) this.head = newNode;
+
+    // -------------------------------------- checking if the head and the tail are pointing to null
+    if (!this.head) this.head = newNode;
     else this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
+
+    return this; // this is the intire linkied list
   }
 
   unshift(value) {
@@ -26,9 +30,44 @@ class LInkedList {
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
+
+    return this; // this is the entire underlined linkedlist
+  }
+
+  pop() {
+    if (!this.head) return null;
+
+    this.length--;
+
+    if (!this.head.next) {
+      this.head = null;
+      this.tail = this.head;
+
+      return null;
+    }
+
+    let curNode = this.head;
+
+    while (true) {
+      if (!curNode.next.next) {
+        this.tail = curNode;
+        this.tail.next = null;
+        break;
+      }
+
+      curNode = curNode.next;
+    }
+
+    return this;
   }
 }
 
 let myLinkedList = new LInkedList(4);
+
+myLinkedList.push(5);
+myLinkedList.push(7);
+myLinkedList.unshift(3);
+
+myLinkedList.pop();
 
 console.log(myLinkedList);
